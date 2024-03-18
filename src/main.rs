662314de957658cb86608fe759b90cb36ab9ba49
BurdenL <burdenlee@outlook.com>
2024-03-19 00:19:40 +0800

    update md5

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,9 @@
 fn main() {
     println!("Hello, world!");
+
+    let digest = md5::compute(b"abcdefghijklmnopqrstuvwxyz");
+
+
+    println!("md5 {:?}", digest);
+    assert_eq!(format!("{:x}", digest), "c3fcd3d76192e4007dfb496cca67e13b");
 }
